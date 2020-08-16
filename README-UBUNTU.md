67e8b4c806d92213ba7d0414a5d83ed94df8e1dc
skytomo <tomo1kawa.fullerene@gmail.com>
2020-08-16 20:13:58 +0900

    READMEとPowerShellの修正

--- README-UBUNTU.md
+++ README-UBUNTU.md
@@ -1,62 +1,62 @@
-# 前提条件をインストールする
+# Install prequisites
 
 ```
 sudo apt-get install wget xsltproc xmlto fonts-dejavu fonts-linuxlibertine unifont ruby-full zip unzip default-jdk`
 gem install bundler
 bundle install
 
-#epub libをインストールします：
+#install epub lib:
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 #Xvfb and xvfb-run (fake X for calibre) or a running X server session -- MAYBE NOT ACTUALLY NEEDED
 
 sudo apt-get install python-pip
 sudo apt-get install xvfb xserver-xephyr vnc4server
 sudo pip install pyvirtualdisplay
-# オプション
+# optional
 sudo apt-get install python-pil scrot
 sudo pip install pyscreenshot
 
-# プラットフォーム用のPrinceを https://www.princexml.com/download/ からインストールします
-# 例：
+# install Prince for your platform from https://www.princexml.com/download/ e.g.:
 wget https://www.princexml.com/download/prince_12.5-1_ubuntu18.04_amd64.deb
 sudo apt install ./prince_12.5-1_ubuntu18.04_amd64.deb
 ```
 
 
 
-他のライブラリが必要な場合があります。 問題を見つけたら、開いてください。
+There might be other libs needed. Please, open an issue once you find it.
 
-# 編纂
+# Compilation
 
-すべてのバージョンを作成するには以下のコマンドを打ちます：
+To make all the versions do:
 
 `./cll_build`
 
-最終結果は、さまざまな場所に散在する build/ ディレクトリの下に配置されます。
-最終出力のみを別のディレクトリにコピーする場合は、-aオプションを使用できます。
-たとえば、次のようなコマンドを打ちます：
+The final results will end up under the build/ directory, scattered
+about in various places.  If you would like the final outputs only
+to be copied to another directory, you can use the -a option, so for example:
 
 `./cll_build -a output/`
 
-とコマンドを打った場合、すべての出力を output/ ディレクトリに配置しますが、
+would put all the outputs in the output/ directory, whereas
 
 `./cll_build -a ~/public_html/cll_build/`
 
-とコマンドを打った場合、あなたの個人的なウェブスペースにそれらを置きます。
+would put them in your personal webspace.
 
-完全なビルドの実行にはかなりの時間がかかります（おそらく少なくとも1時間）。
-迅速なテストのためにたった1つの章でそれを行うには以下のコマンドを打ちます：
+Running a complete build takes quite a while (like probably at least
+an hour).  To do it for just one chapter for faster testing:
 
 `./cll_build -t chapters/05.xml`
 
-以下のコマンドは本全体を行いますが、はるかに高速です：
+This does the whole book but is also much faster:
 
 `./cll_build -t`
 
-以下のような、-Tで指定された多くのサブターゲットもあります：
+There are many possible sub-targets as well, which are specified
+with -T, such as:
 
 `./cll_build -t -T pdf`
 
-次の方法でターゲットの完全なリストを取得できます：
+You can get a complete list of targets via:
 
 `./cll_build -h`
--- run_container.ps1
+++ run_container.ps1
@@ -7,61 +7,63 @@
 # that is in their path first and has whatever they need; we're
 # going to stop trying to do it for them.
 # 
-CONTAINER_BIN=${CONTAINER_BIN:-$(which podman 2>/dev/null)}
-CONTAINER_BIN=${CONTAINER_BIN:-$(which docker 2>/dev/null)}
-
-if (Invoke-Expression $CONTAINER_BIN info > nul 2>&1) {
-  # Everything is working; no-op
-  CONTAINER_BIN="$CONTAINER_BIN"
+if (![string]::IsNullOrEmpty($(where.exe podman 2>$null))) {
+  $CONTAINER_BIN='podman'
+} elseif (![string]::IsNullOrEmpty($(where.exe docker 2>$null))) {
+  $CONTAINER_BIN='docker'
 } else {
   Write-Output "I can't get a working container system.  You need to either have podman (preferred) or docker installed and running for this build system to work.  I have tried both \"podman info\" and \"docker info\", with and without sudo."
-  exit 1
+  exit 1  
 }
+
 
 # Manually pull any -a or -A arguments so we can use them to build
 # volume-sharing options into our container run call.  This assumes
 # arguments are well-formed; *shrug*
-args=( "$@" )
-extra_vols=()
-extra_dirs=()
-for index in ${!args[@]}
-do
-  arg=${args[$index]}
-  if [ "$arg" = '-a' -o "$arg" = '-A' ]
-  then
-    nextarg=${args[$index+1]}
-    nextargfile="/tmp/$(echo "${args[$index+1]}" | tr -c 'a-zA-Z0-9-' '_' | sed -e 's/^_*//' -e 's/_*$//' )"
-    extra_vols+=("-v" "$nextarg:$nextargfile")
-    extra_dirs+=("$nextarg")
-    args[$index+1]="$nextargfile"
-  fi
-done
+$args = @("$@")
+$extra_vols = @()
+$extra_dirs = @()
+$args | ForEach-Object {$index = 0} {
+  if ("$_" -eq '-a' -or "$_" -eq '-A') {
+    $nextarg="$args[$index+1]"
+    $nextargfile="/tmp/$(echo "$(args[$index+1])" | tr -c 'a-zA-Z0-9-' '_' | sed -e 's/^_*//' -e 's/_*$//' )"
+    $extra_vols += @("-v", "${nextarg}:${nextargfile}")
+    $extra_dirs += @("$nextarg")
+    $args[$index+1] = "$nextargfile"
+  }
+  $index++
+}
 
-Invoke-Expression $CONTAINER_BIN Stop-Process cll_build >/dev/null 2>&1
-Invoke-Expression $CONTAINER_BIN Remove-Item cll_build >/dev/null 2>&1
+Invoke-Expression "$CONTAINER_BIN kill cll_build" 2>$null
+Invoke-Expression "$CONTAINER_BIN rm cll_build" 2>$null
 
-dir=$(readlink -f $(dirname $0))
+$dir = "/$(Convert-Path .)".Replace(":", "").Replace("\", "/");
 
 # FOR TESTING; forces complete container rebuild
 # $CONTAINER_BIN build --no-cache -t lojban/cll_build -f Dockerfile .
 # $CONTAINER_BIN rmi lojban/cll_build
+$TmpFile = New-TemporaryFile
 Write-Output "Running container image build; this may take a while."
-Write-Output
-Invoke-Expression $CONTAINER_BIN build -t lojban/cll_build -f Dockerfile . >/tmp/rc.$$ 2>&1 || {
+Write-Output `n
+if ($?) {
+  Invoke-Expression "$CONTAINER_BIN build -t lojban/cll_build -f Dockerfile ." 2>$TmpFile
+} else {
   Write-Output "Container image build failed.  Here's the output: "
-  Write-Output
-  Get-Content /tmp/rc.$$
-  Write-Output
-  Write-Output
+  Write-Output `n
+  Get-Content $TmpFile
+  Write-Output `n
+  Write-Output `n
   Write-Output "Container image build failed.  Output is above."
-  Write-Output
-  Write-Output
-  Remove-Item -f /tmp/rc.$$
+  Write-Output `n
+  Write-Output `n
+  Remove-Item -f $TmpFile
   exit 1
 }
 
-Remove-Item -f /tmp/rc.$$
+Remove-Item -Path $TmpFile
 
-Invoke-Expression $CONTAINER_BIN run --name cll_build \
-  --userns=keep-id -v "$(Convert-Path .):/srv/cll" "${extra_vols[@]}" -it lojban/cll_build \
-  /bin/bash -c "cd /srv/cll ; ./cll_build ${args[*]}"
+# ここまでしか動くことを確認していません
+Write-Output "$CONTAINER_BIN run --rm --name cll_ja_build -v ${dir}:/srv/cll ${extra_vols[@]} -it lojban/cll_ja_build /bin/bash -c `"cd /srv/cll ; ./cll_build ${args[*]}`""
+exit 1
+
+Invoke-Expression "$CONTAINER_BIN run --rm --name cll_ja_build -v ${dir}:/srv/cll ${extra_vols[@]} -it lojban/cll_ja_build /bin/bash -c `"cd /srv/cll ; ./cll_build ${args[*]}`""
